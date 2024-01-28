a5b84861b973905f04250c54989eaf543902df69
Nikita Karasyov <nikitakarasev177@gmail.com>
2024-01-28 23:36:52 +0300

    add comments in main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+# импортирует модуль subprocess
 import subprocess
 
 
+# класс отвечающий за отображение информации о последних коммитах
 class SimpleGit:
+    # выбор директории
     def __init__(self, git_dir='.'):
         self.git_dir = git_dir
 
+    # показывает историю изменений
     def show(self, branch='main'):
         return self.command(f'git show {branch}')
 
+    # запускает команду
     def command(self, git_cmd):
         try:
             output = subprocess.check_output(git_cmd, shell=True, stderr=subprocess.STDOUT, text=True)
@@ -15,7 +20,7 @@ class SimpleGit:
         except subprocess.CalledProcessError as e:
             return e.output.strip()
 
-
+# корневая директория
 git = SimpleGit('.')
 commit_info = git.show()
 print(commit_info)
